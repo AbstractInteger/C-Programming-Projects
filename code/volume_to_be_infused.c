0503bbcbd52329c98fd86c0c6e0152f3ece10915
Jesse Euresti <jesseeuresti@lavabit.com>
2025-01-23 23:53:27 -0600

    First draft of volume to be infused code

--- code/volume_to_be_infused.c
+++ code/volume_to_be_infused.c
@@ -1,2 +1,35 @@
 /*output information for the lables the hospital pharmacy places on the bags of iv*/
 
+//preprocessor files
+#include <stdio.h>
+#include <string.h>
+
+//clear buffer function
+void clearBuffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
+
+int main() {
+
+    //declare char array for medication name
+    char medicationName[15] = {};
+    
+    //declare medication volume varaible
+    int medicationVolume = -1;
+
+    //get user to input medication name
+    printf("enter the name of the medication: \n");
+    fgets(medicationName, sizeof(medicationName), stdin);
+
+    clearBuffer();
+
+    printf("%s", medicationName);
+
+
+
+
+
+
+    return 0;
+}
